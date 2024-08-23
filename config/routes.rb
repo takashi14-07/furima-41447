fc088c5ccf1457f16e2ef1440c152146a83d32f4
takashi14-07 <yamataka0421@icloud.com>
2024-08-23 17:23:12 +0900

    デプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'items/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+root to: "items#index"
   # root "articles#index"
 end
