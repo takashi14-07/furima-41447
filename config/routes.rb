c17a6ed2abdf700c4b3cc9cabb4e41c58f4c9146
takashi14-07 <yamataka0421@icloud.com>
2024-09-02 21:14:27 +0900

    ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   devise_for :users
   get 'items/index'
-  resources :users, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 root to: "items#index"
