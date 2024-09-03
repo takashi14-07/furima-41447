c679f1ca4aac8a30ddbdbbaddd7a18e6a5a1b454
takashi14-07 <yamataka0421@icloud.com>
2024-09-03 18:19:56 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,21 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birthday, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'must contain @' }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'must include both letters and numbers' }
+  attr_accessor :password_confirmation
+
+  validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :birthday, presence: true
 end
