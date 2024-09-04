5fe3d92ab84e4bc915080cb701ed80f65be03ca5
takashi14-07 <yamataka0421@icloud.com>
2024-09-04 16:49:02 +0900

    ユーザー管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,12 @@ class User < ApplicationRecord
     validates :nickname
     validates :birthday
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英数字混在にしてください' }
-    validates :first_name
-    validates :last_name
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: '全角文字を使用してください' }
     validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
     validates :last_name_kana,  format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
     validates :first_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
     validates :last_name_kana, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: '全角カタカナを使用してください' }
-    validates :birthday
     validates :password, format: { without: /[^\x00-\x7F]/, message: 'は全角文字を含めることはできません' }
-    validates :email
   end
 end
