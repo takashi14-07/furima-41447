228659512a4e25da39ba0215a4c55989f8c32dc4
takashi14-07 <yamataka0421@icloud.com>
2024-09-08 17:54:07 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,10 +23,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shipping_id, :shipping_from_id,
                                  :shipping_date_id, :price).merge(user_id: current_user.id)
   end
-
-  def require_login
-    return if current_user.id == @item.user_id
-
-    redirect_to action: :index
-  end
 end
