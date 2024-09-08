8b94e0438d5b025c4b9e3fab5b4696b13b0bc5f9
takashi14-07 <yamataka0421@icloud.com>
2024-09-08 17:35:25 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,35 +17,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-    return unless @item.order.present?
+ 
 
-    redirect_to root_path
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(params[:id])
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @item.destroy
-    redirect_to root_path
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :shipping_id, :shipping_from_id,
-                                 :shipping_date_id, :price).merge(user_id: current_user.id)
-  end
-
-  def require_login
-    return if current_user.id == @item.user_id
-
-    redirect_to action: :index
-  end
-end
+    
+  
