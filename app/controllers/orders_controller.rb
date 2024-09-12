4733b1b227cbf2fc0c0c2d91394af96c3e2883bc
takashi14-07 <yamataka0421@icloud.com>
2024-09-12 16:09:46 +0900

    購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: [:index]
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def new
@@ -13,14 +16,15 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     @item = Item.find(params[:item_id])
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    
+    if @order_address.valid?
+      @order_address.save
+      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
       card: order_params[:token], # カードトークン
       currency: 'jpy' # 通貨の種類（日本円）
     )
-    if @order_address.valid?
-      @order_address.save
       redirect_to root_path
     else
       gon.public_key = ENV['PAYJP_PUBLIC_KEY']
